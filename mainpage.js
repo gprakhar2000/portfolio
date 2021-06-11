7af24289574f11c5e6fcdc536f9e20e64e12ac6c
Prakhar Gupta <gprakhar2000@gmail.com>
2021-06-11 15:31:26 +0530

    changes

--- mainpage.js
+++ mainpage.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
 $(window).load(function() {
     $('.loader').hide();
-=======
-$(window).load(function() {
-    $('.loader').hide();
->>>>>>> c434dab82c278934ea310031aa50f8b8aeb02608
   });
